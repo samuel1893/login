d2727d05f73a99e7ceb6665c4844d74bcf7d4d5d
samuell1893 <samuellemos05@gmail.com>
2022-07-08 10:16:11 +0100

    Revert "Revert "no message""
    This reverts commit 7b823e1a3e13cf461f40b78bf752f38c054d6a46.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const User = require("./models/User");
 const db = require("./models/db");
-const crypto = require("crypto")
+const crypto = require("crypto");
+let alert = require('alert'); 
+
 
 const port = 3310;
 var path = require("path");
@@ -12,46 +14,98 @@ const sequelize = require("sequelize");
 const { userInfo } = require("os");
 const app = express();
 
-app.use(session({secret:"hfewiu2hgfoewhgf"}));
+const header = JSON.stringify({
+    'alg': 'HS256',
+    'typ': 'JWT'
+});
+
+app.use(session({secret:"ewt457dfhdf"}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.engine("html", require("ejs").renderFile);
-app.set("view engine", "ejs");
+app.set("view engine", "html");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("views",path.join(__dirname, "/views"));
 
-app.get("/login",(req, res) => {
-    res.render("login");
-})
+
 
 app.get("/registrar",(req, res) => {
     res.render("registrar");
 })
-
+app.get("/login",(req, res) => {
+    res.render("login");
+})
+app.get("/inicio",(req, res) => {
+    res.render("inicio");
+})
 
 app.post("/registrar", async(req, res) => {
     var nome= req.body.name;
-    var email= req.body.email;
+    var email= req.body.email; 
     var senha= req.body.password;
     var cargo = req.body.role;
-    let hash = crypto.createHash('md5').update("senha").digest("hex")
-    await User.create({nome: nome, email: email, password: hash, cargo: cargo});
+    let hash = crypto.createHash('md5').update(senha).digest("hex");
+   
+    await User.create({nome: req.body.name, password: hash, email: email, cargo: cargo});
+    console.log(nome, email, hash, cargo);
  
-    res.render("sucesso");
+    res.render("inicio");
 })
 app.post("/login", async(req, res) => {
     var email= req.body.email;
     var senha= req.body.password;
-    let hash = crypto.createHash('md5').update("senha").digest("hex")
-    db.query("SELECT * FROM users WHERE password ='"+hash+"' AND email = '"+email+"'",  function (err, results){
-        if(results.length > 0){
-            res.render("../views/sucesso.ejs");
-        }
-        else{
-            console.log("erro");
-        }
-    });
+    let hash = crypto.createHash('md5').update(senha).digest("hex");
 
-        
+    const payload = JSON.stringify({
+        'email': email,
+        'password': hash
+    });
+    const base64Header = Buffer.from(header).toString('base64').replace(/=/g, '');
+    const base64Payload = Buffer.from(payload).toString('base64').replace(/=/g, '');
+    const secret = 'my-custom-secret';
+
+    const data = base64Header + '.' + base64Payload;
+
+    const signature = crypto
+        .createHmac('sha256', secret)
+        .update(data)
+        .digest('base64');
+
+    const signatureUrl = signature
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '')
+
+
+    let user = await User.findOne({
+        where: {
+            email: email,
+            password: hash
+        }
+    })
+    if (user!= null){
+        res.render("inicio");
+        alert('Welcome');
+    }
+    else{
+        alert('Incorrect email or password.')
+        res.render("login");
+    }
+    return(signatureUrl);       
+})
+
+app.get("/help",(req, res) => {
+    res.render("help");
+})
+
+app.post("/help",(req, res) => {
+    if(signatureUrl !== null && signatureUrl !== '') {
+        console.log(signatureUrl);
+        res.render("help");
+    }
+    else{
+        res.render("registrar")
+        alert('Unauthorized')
+    }
 })
 
 app.listen(port, () => {
